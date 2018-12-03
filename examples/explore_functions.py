7f56c1bd57839516aeda5c1ed68fa0c8854f8bc0
Josselin <josselin@trailofbits.com>
2018-12-03 12:28:01 +0000

    Minor

--- examples/explore_functions.py
+++ examples/explore_functions.py
@@ -1,7 +1,7 @@
 import pprint
 from evm_cfg_builder.cfg import CFG
 
-with open('token-runtime0.5.evm') as f:
+with open('token-runtime.evm') as f:
     runtime_bytecode = f.read()
 
 cfg = CFG(runtime_bytecode)

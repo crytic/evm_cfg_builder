02772bf568d642fe2eaa5a08e3751829c84fa68a
Josselin <josselin@trailofbits.com>
2021-03-29 19:35:58 +0200

    0.3.1

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     description='EVM cfg builder written in Python 3.',
     url='https://github.com/trailofbits/evm_cfg_builder',
     author='Trail of Bits',
-    version='0.3.0',
+    version='0.3.1',
     packages=find_packages(),
     python_requires='>=3.6',
-    install_requires=['pyevmasm>=0.1.1', 'crytic-compile>=0.1.5'],
+    install_requires=['pyevmasm>=0.1.1', 'crytic-compile>=0.1.13'],
     license='AGPL-3.0',
     long_description=open('README.md').read(),
     entry_points={
